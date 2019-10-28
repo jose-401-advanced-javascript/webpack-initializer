70e0a511aeebc618476ff2a7a2fb52bf1247b6d1
Jose <j.a.ojeda@live.com>
2019-10-28 14:34:14 -0700

    wrote writer.js

--- writer/writer.js
+++ writer/writer.js
@@ -1,11 +1,17 @@
+const fs = require('fs');
+
 const write = (str, path) => {
-  console.log(str, path);
-  
+  fs.writeFileSync(path, str);
+  console.log(path);
 };
 
 const writeJson = (obj, path) => {
-  console.log(obj, path);
+  const json = JSON.stringify(obj);
+  fs.writeFileSync(path, json);
+  console.log(path);
 };
+
+writeJson({ test: 'abc' }, './test.txt');
 
 module.exports = {
   write,
